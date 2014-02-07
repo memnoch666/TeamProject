1513313f8a91bdf581d2c45ecb90101f313f7cde
Ralph Vickery <memnoch@gmail.com>
2014-02-07 13:49:59 -0800

    Mase Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeamProject::Application.config.secret_key_base = '7fc0e0be324510e997274ddfb17fcf7f416b22fc8a9de0ecd828f565d1d66335645d494f4d29c3d86ad012c27e758aa50359bc18b98ba424d408f5c1b503b530'
